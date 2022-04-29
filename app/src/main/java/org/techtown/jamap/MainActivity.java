0600b91f220dcb7b8bc7d07f0e027079762113cf
YeongJoo Ryu <84553046+sm-ryj@users.noreply.github.com>
2022-04-29 14:04:56 +0900

    ryj - map 현재위치

--- app/src/main/java/org/techtown/jamap/MainActivity.java
+++ app/src/main/java/org/techtown/jamap/MainActivity.java
@@ -12,19 +12,31 @@ import android.widget.Toast;
 
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.CameraUpdate;
+import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.NaverMapOptions;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.util.FusedLocationSource;
 
 public class MainActivity extends FragmentActivity
         implements OnMapReadyCallback {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        locationSource =
+                new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
 
         FragmentManager fm = getSupportFragmentManager();
         MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map_fragment);
@@ -39,16 +51,63 @@ public class MainActivity extends FragmentActivity
     @UiThread
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
-        LatLng coord = new LatLng(37.5670135, 126.9783740);
 
-        Toast.makeText(this.getApplicationContext(),
-                "위도: " + coord.latitude + ", 경도: " + coord.longitude,
-                Toast.LENGTH_SHORT).show();
+        this.naverMap = naverMap;
+        naverMap.setLocationSource(locationSource);
+
+//        CameraUpdate cameraUpdate = CameraUpdate.scrollTo(new LatLng(37.5666102, 126.9783881));
+//        naverMap.moveCamera(cameraUpdate);
+
+//        LatLng coord = new LatLng(37.5670135, 126.9783740);
+//
+//        Toast.makeText(this.getApplicationContext(),
+//                "위도: " + coord.latitude + ", 경도: " + coord.longitude,
+//                Toast.LENGTH_SHORT).show();
 
         NaverMapOptions options = new NaverMapOptions()
                 .camera(new CameraPosition(new LatLng(35.1798159, 129.0750222), 8))
                 .mapType(NaverMap.MapType.Terrain);
+
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+
+        UiSettings uiSettings = naverMap.getUiSettings();
+        uiSettings.setCompassEnabled(true);
+        uiSettings.setZoomControlEnabled(true);
+        uiSettings.setLocationButtonEnabled(true);
+
+        naverMap.setOnMapClickListener((point, coord) ->
+                Toast.makeText(this, coord.latitude + ", " + coord.longitude,
+                        Toast.LENGTH_SHORT).show());
+        naverMap.setOnMapClickListener((point, coord) ->
+                Toast.makeText(this, "지도 클릭", Toast.LENGTH_SHORT).show()
+        );
+
+        naverMap.setOnSymbolClickListener(symbol -> {
+            if ("서울특별시청".equals(symbol.getCaption())) {
+                Toast.makeText(this, "서울시청 클릭", Toast.LENGTH_SHORT).show();
+                // 이벤트 소비, OnMapClick 이벤트는 발생하지 않음
+                return true;
+            }
+            // 이벤트 전파, OnMapClick 이벤트가 발생함
+            return false;
+        });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) { // 권한 거부됨
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
+    }
+
+
 }
 
 //        extends AppCompatActivity {
